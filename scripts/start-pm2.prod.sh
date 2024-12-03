55bcf4da761297648976c78a82a13b8eba1c44d7
Gegham Khachatryan <gegham.k90@gmail.com>
2024-12-03 11:07:06 +0400

    feat: prod configs

---
+++ scripts/start-pm2.prod.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+if [ -f ".env" ]; then
+  set -a
+  source .env set
+  set +a
+fi
+
+docker-compose up -d
+
+LIB_NAMES=$(npx lerna list --all --parseable --toposort --long | grep libs | sed -E "s/[^:]+:([^:]+).+/\1/" | paste -d , -s -)
+
+# build libs
+npx lerna run --sort --scope={$LIB_NAMES} build
+
+pm2 start pm2.config.prod.yml --update-env
